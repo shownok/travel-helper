df6e9667ecdacbc3f7779456275f83bd44d88245
shownok <shownokferdous@gmail.com>
2022-10-03 18:18:32 +0600

    my update

---
+++ src/api/index.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+  try {
+    const {
+      data: { data },
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          "X-RapidAPI-Key":
+            "e9ba2363e2msh9065f01e833e601p1d02f8jsn74a8cf9909a0",
+          "X-RapidAPI-Host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
